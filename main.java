53dcedd277781cf60ab72a2a3d4ce71240c42cfd
HorraceMuendo <horracemuendo34@gmail.com>
2023-05-25 23:30:51 +0300

    project init

---
+++ main.java
@@ -0,0 +1,24 @@
+﻿import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+enum Suit {
+    SPADES,
+    HEARTS,
+    DIAMONDS,
+    CLUBS
+}
+
+enum Rank {
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+}
+
+class Card {
+    private Suit suit;
+    private Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
