7e3f9961cfc6c1c50928a7b418067df21f25fd41
HorraceMuendo <horracemuendo34@gmail.com>
2023-05-27 23:13:11 +0300

    wrote the deck func

--- StreetPoker/src/Deck.java
+++ StreetPoker/src/Deck.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
+    private List<Card> cards;
+    public Deck(){
+        cards=new ArrayList<>();
+        for (Suit suit : Suit.values()){
+            for (Rank rank : Rank.values()){
+                cards.add(new Card(rank,suit))
+            }
+        }
+    }
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+    public Card drawCard (){
+        if (cards.isEmpty()){
+            return null;
+        }
+        return cards.remove(cards.size()-1);
+    }
+
+
 }
